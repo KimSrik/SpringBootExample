0b5b276f4a9a7b98e43c2248383c3ae3fd5d53e1
Kim Srik <rltmrl2@gmail.com>
2026-01-19 10:22:44 +0900

    first commit(init)

---
+++ Gimpo/src/test/java/com/example/gimpo/GimpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gimpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GimpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
