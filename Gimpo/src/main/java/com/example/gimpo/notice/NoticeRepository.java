c18c44af341a309d4a0b86cc9eeaee70742c06a3
Kim Srik <rltmrl2@gmail.com>
2026-01-19 11:22:30 +0900

    Repository-extends Jpa added

---
+++ Gimpo/src/main/java/com/example/gimpo/notice/NoticeRepository.java
@@ -0,0 +1,7 @@
+package com.example.gimpo.notice;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<Notice, Integer> {
+	
+}
---
+++ Gimpo/src/main/java/com/example/gimpo/opinion/OpinionRepository.java
@@ -0,0 +1,7 @@
+package com.example.gimpo.opinion;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OpinionRepository extends JpaRepository<Opinion, Integer> {
+
+}
